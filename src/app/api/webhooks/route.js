293bc79333e2310f732aa90b7b1b82f5ef2e4071
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-11-16 18:48:57 +0200

    add mongodb

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -49,10 +51,54 @@ export async function POST(req) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
-  const eventType = evt.type;
+  const { id } = evt?.data;
+  const eventType = evt?.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
+
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, first_name, last_name, image_url, email_addresses, username } =
+      evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses,
+        username
+      );
+      if (user && eventType === "user.created") {
+        try {
+          await clerkClient.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+              isAdmin: user.isAdmin,
+            },
+          });
+        } catch (error) {
+          console.log("Error updating user metadata:", error);
+        }
+      }
+    } catch (error) {
+      console.log("Error creating or updating user:", error);
+      return new Response("Error occured", {
+        status: 400,
+      });
+    }
+  }
+
+  if (eventType === "user.deleted") {
+    const { id } = evt?.data;
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log("Error deleting user:", error);
+      return new Response("Error occured", {
+        status: 400,
+      });
+    }
+  }
 
   return new Response("", { status: 200 });
 }
---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+let initialized = flase;
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+  if (initialized) {
+    console.log("mongodb connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "next-blog",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+    initialized = true;
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+  }
+};
