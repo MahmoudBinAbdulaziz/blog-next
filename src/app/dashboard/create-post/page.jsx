728c1be67bb8d897905cc463f210068d6153a98c
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-11-17 21:20:59 +0200

    create and get post

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -9,9 +9,77 @@ import { useState } from "react";
 const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 // https://dev.to/a7u/reactquill-with-nextjs-478b
 import "react-quill-new/dist/quill.snow.css";
-function page() {
+import { useRouter } from "next/navigation";
+function Page() {
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [uploading, setUploading] = useState(false);
   const { isSignedIn, user, isLoaded } = useUser();
-
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const addImageToPost = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      // Convert file to base64 for Imgur upload
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImageFileUrl(reader.result); // This will be used for the Imgur API
+      };
+    }
+  };
+  // console.log(imageFileUrl);
+  const handleUpload = async () => {
+    try {
+      if (!imageFileUrl) {
+        setImageUploadError("Please select an image");
+        return;
+      }
+      setImageUploadError(null);
+      setUploading(true);
+      const response = await fetch("/api/uploadToImgur", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ imageData: imageFileUrl }),
+      });
+      setUploading(false);
+      // console.log(response);
+      const data = await response.json();
+      setFormData({ ...formData, image: data.imageUrl });
+      setImageFileUrl(null);
+    } catch (error) {
+      setImageUploadError("Image upload failed");
+      setUploading(false);
+    }
+  };
+  const router = useRouter();
+  console.log(formData);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userMongoId: user.publicMetadata.userMongoId,
+        }),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+      if (res.ok) {
+        setPublishError(null);
+        router.push(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError("Something went wrong");
+    }
+  };
   if (!isLoaded) {
     return null;
   }
@@ -22,7 +90,7 @@ function page() {
         <h1 className="text-center text-3xl my-7 font-semibold">
           Create a post
         </h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={submitHandler}>
           <div className="flex flex-col gap-4 sm:flex-row justify-between">
             <TextInput
               type="text"
@@ -30,14 +98,14 @@ function page() {
               required
               id="title"
               className="flex-1"
-              //   onChange={(e) =>
-              //     setFormData({ ...formData, title: e.target.value })
-              //   }
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
             />
             <Select
-            //   onChange={(e) =>
-            //     setFormData({ ...formData, category: e.target.value })
-            //   }
+              onChange={(e) =>
+                setFormData({ ...formData, category: e.target.value })
+              }
             >
               <option value="uncategorized">Select a category</option>
               <option value="javascript">JavaScript</option>
@@ -46,52 +114,38 @@ function page() {
             </Select>
           </div>
           <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-            <FileInput
-              type="file"
-              accept="image/*"
-              //   onChange={(e) => setFile(e.target.files[0])}
-            />
+            <FileInput type="file" accept="image/*" onChange={addImageToPost} />
             <Button
               type="button"
               gradientDuoTone="purpleToBlue"
               size="sm"
               outline
-              //   onClick={handleUpdloadImage}
-              //   disabled={imageUploadProgress}
+              onClick={handleUpload}
+              disabled={uploading}
             >
-              {/* {imageUploadProgress ? (
-                <div className="w-16 h-16">
-                  <CircularProgressbar
-                    value={imageUploadProgress}
-                    text={`${imageUploadProgress || 0}%`}
-                  />
-                </div>
-              ) : (
-                "Upload Image"
-              )} */}
               Upload Image
             </Button>
           </div>
 
-          {/* {imageUploadError && (
+          {imageUploadError && (
             <Alert color="failure">{imageUploadError}</Alert>
-          )} */}
-          {/* {formData.image && (
+          )}
+          {formData.image && (
             <img
               src={formData.image}
               alt="upload"
               className="w-full h-72 object-cover"
             />
-          )} */}
+          )}
 
           <ReactQuill
             theme="snow"
             placeholder="Write something..."
             className="h-72 mb-12"
             required
-            // onChange={(value) => {
-            //   setFormData({ ...formData, content: value });
-            // }}
+            onChange={(value) => {
+              setFormData({ ...formData, content: value });
+            }}
           />
           <Button type="submit" gradientDuoTone="purpleToPink">
             Publish
@@ -108,4 +162,4 @@ function page() {
   }
 }
 
-export default page;
+export default Page;
